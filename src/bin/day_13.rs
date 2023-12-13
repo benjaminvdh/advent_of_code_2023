5d9925658fdb1e4370250491dbb5af9a1eb5d327
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2023-12-13 22:05:14 +0100

    Day 13 part 1

---
+++ src/bin/day_13.rs
@@ -0,0 +1,139 @@
+struct Solver;
+
+impl aoc::Solver for Solver {
+    type Input = Vec<Vec<Vec<char>>>;
+    type Output1 = usize;
+    type Output2 = usize;
+
+    fn parse(input: &str) -> Self::Input {
+        input
+            .split("\n\n")
+            .map(|split| split.lines().map(|line| line.chars().collect()).collect())
+            .collect()
+    }
+
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input.iter().map(|pattern| get_pattern_score(pattern)).sum()
+    }
+
+    fn part_2(_input: &Self::Input) -> Self::Output2 {
+        todo!()
+    }
+}
+
+fn get_pattern_score(pattern: &[Vec<char>]) -> usize {
+    let y_score = (1..pattern.len())
+        .filter_map(|split| rows_reflect_at(pattern, split))
+        .max()
+        .unwrap_or(0);
+
+    let x_score = (1..pattern[0].len())
+        .filter_map(|split| cols_reflect_at(pattern, split))
+        .max()
+        .unwrap_or(0);
+
+    y_score * 100 + x_score
+}
+
+fn cols_reflect_at(lines: &[Vec<char>], vertical_split: usize) -> Option<usize> {
+    let mut forward = vertical_split;
+    let mut backward = vertical_split - 1;
+
+    loop {
+        if lines.iter().any(|line| line[forward] != line[backward]) {
+            return None;
+        }
+
+        if forward == lines[0].len() - 1 || backward == 0 {
+            break;
+        }
+
+        forward += 1;
+        backward -= 1;
+    }
+
+    Some(vertical_split)
+}
+
+fn rows_reflect_at(lines: &[Vec<char>], horizontal_split: usize) -> Option<usize> {
+    let mut forward = horizontal_split;
+    let mut backward = horizontal_split - 1;
+
+    loop {
+        if lines[forward] != lines[backward] {
+            return None;
+        }
+
+        if forward == lines.len() - 1 || backward == 0 {
+            break;
+        }
+
+        forward += 1;
+        backward -= 1;
+    }
+
+    Some(horizontal_split)
+}
+
+fn main() {
+    aoc::run::<Solver>();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_input() -> <Solver as aoc::Solver>::Input {
+        vec![
+            vec![
+                vec!['#', '.', '#', '#', '.', '.', '#', '#', '.'],
+                vec!['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+                vec!['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+                vec!['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+                vec!['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+                vec!['.', '.', '#', '#', '.', '.', '#', '#', '.'],
+                vec!['#', '.', '#', '.', '#', '#', '.', '#', '.'],
+            ],
+            vec![
+                vec!['#', '.', '.', '.', '#', '#', '.', '.', '#'],
+                vec!['#', '.', '.', '.', '.', '#', '.', '.', '#'],
+                vec!['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+                vec!['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+                vec!['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+                vec!['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+                vec!['#', '.', '.', '.', '.', '#', '.', '.', '#'],
+            ],
+        ]
+    }
+
+    #[test]
+    fn parsing() {
+        let input = r"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+        assert_eq!(<Solver as aoc::Solver>::parse(input), get_input());
+    }
+
+    #[test]
+    fn part_1() {
+        assert_eq!(<Solver as aoc::Solver>::part_1(&get_input()), 405);
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(<Solver as aoc::Solver>::part_2(&get_input()), todo!());
+    }
+}
