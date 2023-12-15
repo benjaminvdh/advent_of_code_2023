c41ded8615fbd4f310d6489dbbb6102be8026b8c
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2023-12-15 10:45:02 +0100

    Day 15 part 2

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -1,3 +1,90 @@
+struct Step {
+    label: String,
+    operation: char,
+    focal_length: usize,
+}
+
+impl From<&str> for Step {
+    fn from(string: &str) -> Self {
+        let operation_index = string.chars().position(|c| c == '-' || c == '=').unwrap();
+
+        let label = string[0..operation_index].to_owned();
+
+        let operation = string.chars().nth(operation_index).unwrap();
+
+        let focal_length = if operation == '=' {
+            string.chars().nth(operation_index + 1).unwrap() as usize - '0' as usize
+        } else {
+            0
+        };
+
+        Self {
+            label,
+            operation,
+            focal_length,
+        }
+    }
+}
+
+struct Boxes(Vec<Vec<(String, usize)>>);
+
+impl Boxes {
+    pub fn new() -> Self {
+        Self([vec![]].into_iter().cycle().take(256).collect())
+    }
+
+    pub fn apply(self, step: &Step) -> Self {
+        match step.operation {
+            '=' => self.add(step),
+            '-' => self.remove(step),
+            _ => panic!(),
+        }
+    }
+
+    pub fn get_focusing_power(&self) -> usize {
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(box_index, boxes)| {
+                (box_index + 1)
+                    * boxes
+                        .iter()
+                        .enumerate()
+                        .map(|(lens_index, (_, focal_length))| (lens_index + 1) * focal_length)
+                        .sum::<usize>()
+            })
+            .sum()
+    }
+
+    fn add(mut self, step: &Step) -> Self {
+        let hash = run_hash(&step.label);
+
+        if let Some(lens_box) = self.0[hash]
+            .iter_mut()
+            .find(|lens_box| lens_box.0 == step.label)
+        {
+            lens_box.1 = step.focal_length;
+        } else {
+            self.0[hash].push((step.label.to_owned(), step.focal_length))
+        }
+
+        self
+    }
+
+    fn remove(mut self, step: &Step) -> Self {
+        let hash = run_hash(&step.label);
+
+        if let Some(index) = self.0[hash]
+            .iter()
+            .position(|lens_box| lens_box.0 == step.label)
+        {
+            let _ = self.0[hash].remove(index);
+        }
+
+        self
+    }
+}
+
 struct Solver;
 
 impl aoc::Solver for Solver {
@@ -16,8 +103,12 @@ impl aoc::Solver for Solver {
         input.iter().map(|seq| run_hash(seq)).sum()
     }
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        todo!()
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        input
+            .iter()
+            .map(|step| Step::from(step.as_str()))
+            .fold(Boxes::new(), |boxes, step| boxes.apply(&step))
+            .get_focusing_power()
     }
 }
 
@@ -65,6 +156,6 @@ mod tests {
 
     #[test]
     fn part_2() {
-        assert_eq!(<Solver as aoc::Solver>::part_2(&get_input()), todo!());
+        assert_eq!(<Solver as aoc::Solver>::part_2(&get_input()), 145);
     }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -63,6 +63,11 @@ fn day_14() {
     test(env!("CARGO_BIN_EXE_day_14"), 14);
 }
 
+#[test]
+fn day_15() {
+    test(env!("CARGO_BIN_EXE_day_15"), 15);
+}
+
 fn parse_string(string: &str) -> (&str, &str) {
     let mut splits = string.split_terminator("\0");
     (splits.next().unwrap(), splits.next().unwrap())
